aefc658e221e09bc9d3ce5192784b1a1ee5f263f
pravenaditiyaa <kgnoble21@outlook.com>
2019-03-17 12:29:17 +0530

    first master commit

---
+++ src/app/app.register.js
@@ -0,0 +1,11 @@
+import {Navigation} from 'react-native-navigation';
+
+export function registerScreens() {
+  Navigation.registerComponent('Initializing', () => require('./app.initializing').default);
+  Navigation.registerComponent('Home', () => require('./dashboard/Home').default);
+  Navigation.registerComponent('SignIn', () => require('./userAuthentication/SignIn').default);
+  Navigation.registerComponent('SignUp', () => require('./userAuthentication/SignUp').default);
+  Navigation.registerComponent('Screen2', () => require('./dashboard/Screen2').default);
+  Navigation.registerComponent('Menu', () => require('./menu/menu').default);
+  Navigation.registerComponent('About', () => require('./menu/aboutus').default);
+}
---
+++ src/app/navigation/navigation.js
@@ -0,0 +1,200 @@
+import { Navigation } from 'react-native-navigation'
+
+export const goToAuth = () => Navigation.setRoot({
+  root: {
+    bottomTabs: {
+      id: 'BottomTabsId',
+      children: [
+        {
+          component: {
+            name: 'SignIn',
+            options: {
+              bottomTab: {
+                fontSize: 12,
+                text: 'Sign In',
+                icon: require('../../assets/signin.png')
+              }
+            }
+          },
+        },
+        {
+          component: {
+            name: 'SignUp',
+            options: {
+              bottomTab: {
+                text: 'Sign Up',
+                fontSize: 12,
+                icon: require('../../assets/signup.png')
+              }
+            }
+          },
+        },
+      ],
+    }
+  }
+});
+
+export const goHome = () => Navigation.setRoot({
+  // root: {
+  //   stack: {
+  //     id: 'App',
+  //     children: [
+  //       {
+  //         component: {
+  //           name: 'Home',
+  //         }
+  //       }
+  //   ],
+  //   }
+  // }
+  root: {
+    bottomTabs: {
+      children: [{
+        stack: {
+          children: [{
+            component: {
+              name: 'Home',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'TAB 1'
+                  },
+                  leftButtons: [
+                    {
+                      id: 'nav_user_btn',
+                      icon: require('../../assets/icons/ic_nav_user.png'),
+                      color: 'black'
+                    }
+                  ],
+                  rightButtons: [
+                    {
+                      id: 'nav_logout_btn',
+                      icon: require('../../assets/icons/ic_nav_logout.png'),
+                      color: 'black'
+                    }
+                  ]
+                }
+              }
+            }
+          }],
+          options: {
+            bottomTab: {
+              icon: require('../../assets/icons/ic_tab_home.png'),
+              testID: 'FIRST_TAB_BAR_BUTTON',
+              text: 'Tab1',
+            }
+          }
+        }
+      },
+      {
+        stack: {
+          children: [{
+            component: {
+              name: 'Screen2',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'TAB 2'
+                  },
+                  leftButtons: [
+                    {
+                      id: 'nav_user_btn',
+                      icon: require('../../assets/icons/ic_nav_user.png'),
+                      color: 'black'
+                    }
+                  ],
+                  rightButtons: [
+                    {
+                      id: 'nav_logout_btn',
+                      icon: require('../../assets/icons/ic_nav_logout.png'),
+                      color: 'black'
+                    }
+                  ]
+                }
+              }
+            }
+          }],
+          options: {
+            bottomTab: {
+              icon: require('../../assets/icons/ic_tab_menu.png'),
+              testID: 'SECOND_TAB_BAR_BUTTON',
+              text: 'Tab2',
+            }
+          }
+        }
+      },{
+        stack: {
+          children: [{
+            component: {
+              name: 'Home',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'TAB 1'
+                  },
+                  leftButtons: [
+                    {
+                      id: 'nav_user_btn',
+                      icon: require('../../assets/icons/ic_nav_user.png'),
+                      color: 'black'
+                    }
+                  ],
+                  rightButtons: [
+                    {
+                      id: 'nav_logout_btn',
+                      icon: require('../../assets/icons/ic_nav_logout.png'),
+                      color: 'black'
+                    }
+                  ]
+                }
+              }
+            }
+          }],
+          options: {
+            bottomTab: {
+              icon: require('../../assets/icons/ic_tab_home.png'),
+              testID: 'FIRST_TAB_BAR_BUTTON',
+              text: 'Tab1',
+            }
+          }
+        }
+      },{
+        stack: {
+          children: [{
+            component: {
+              name: 'Menu',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'TAB 1'
+                  },
+                  leftButtons: [
+                    {
+                      id: 'nav_user_btn',
+                      icon: require('../../assets/icons/ic_nav_user.png'),
+                      color: 'black'
+                    }
+                  ],
+                  rightButtons: [
+                    {
+                      id: 'nav_logout_btn',
+                      icon: require('../../assets/icons/ic_nav_logout.png'),
+                      color: 'black'
+                    }
+                  ]
+                }
+              }
+            }
+          }],
+          options: {
+            bottomTab: {
+              icon: require('../../assets/icons/ic_tab_menu.png'),
+              testID: 'FIRST_TAB_BAR_BUTTON',
+              text: 'Tab1',
+            }
+          }
+        }
+      }]
+    }
+  }
+})
